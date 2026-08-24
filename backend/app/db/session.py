6b842d1b9cb26c4424c17b9c71fafd76c8e0a991
TaariqA <taariqasamad@gmail.com>
2026-08-24 19:24:49 -0400

    working on database connection & session creation

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,3 +1,32 @@
+#MODULES TO CREATE ENGINE THAT CONNECTS TO DATABASE & CREATE A SESSION FOR ROUTES TO ACCESS
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+#MODULES TO ACCESS SECRETS IN .ENV
+from dotenv import load_dotenv
+import os
 
-engine = create_engine("postgresql+psycopg2://:memory:", echo=True)
+#assigning .env secret to a variable
+load_dotenv()
+DB_LINK = os.getenv("DB_LINK")
+
+#error catching just in case DB_LINK doesnt exist
+if not DB_LINK:
+    raise ValueError("DB_LINK environment variable is not set")
+
+engine = create_engine(DB_LINK, echo=True)
+
+#Sessions
+sessionLocal = sessionmaker(
+    autoflush=False,
+    bind=engine
+)
+
+#FASTAPI DB access
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+#I HAVE EDITED PYVENV.CFG ON LAPTOP, CHECK ON PC IF VENV & INTERPRETER STILL WORK
