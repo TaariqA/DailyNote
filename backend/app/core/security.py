fbdca69c6c75b92939efa54e4753f27b7fb6583b
TaariqA <taariqasamad@gmail.com>
2026-09-19 14:28:46 -0400

    created logic to hash user pw & authenticate an entered password, passowrds are stored in bytes???? may throw a type mismatch since they are registered as strings throughout the project

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -5,3 +5,19 @@ token validation
 extracting the current user from a request
 checking if the user is authenticated
 optional role/permission checks'''
+import bcrypt
+
+#hashes user pw when account is made
+def password_hash(password: str):
+    passwordInBytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+
+    hash = bcrypt.hashpw(passwordInBytes, salt)
+
+    return hash
+
+def password_authenticate(enteredPw: str, userPwHash: bytes):
+    enteredPwBytes = enteredPw.encode('utf-8')
+    result = bcrypt.checkpw(enteredPwBytes, userPwHash)
+
+    return result
