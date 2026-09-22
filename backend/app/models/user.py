702bef147f677475e8fa1c1405f4d208b032ec20
TaariqA <taariqasamad@gmail.com>
2026-09-22 16:13:55 -0400

    changing the type of the pw column in the user table to reflect that the values are all hashed

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, LargeBinary
 from backend.app.db.base import Base
 
 #Model for the user table, contains id, email, password
@@ -8,4 +8,4 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     email = Column(String, unique=True, index=True, nullable=False)
     name = Column(String, nullable=False)
-    password_hash = Column(String, nullable=False)
+    password_hash = Column(LargeBinary, nullable=False)
